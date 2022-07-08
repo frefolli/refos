84b72b4ad38dd9ce4c83e248fa5f13f25d23b907
frefolli <102532146+frefolli@users.noreply.github.com>
2022-07-08 12:04:42 +0200

    added check functions to boot

--- inc/boot/boot.hpp
+++ inc/boot/boot.hpp
@@ -2,6 +2,7 @@
 #define __BOOT_HPP__
 
 #include "../video/video.hpp"
+#include "../libc/stdbool.hpp"
 
 namespace boot {
     typedef struct __attribute__((packed)) {
@@ -14,6 +15,10 @@ namespace boot {
         meta_t meta;
         video::screen_t screen;
     } info_t;
+    
+    bool checkMagic(uint64_t magic);
+    
+    bool checkAddress(uint64_t address);
 
     void readHeader(uint8_t* address, info_t* boot_info);
 
