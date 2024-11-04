1e44249ec5a769da9fb2d1bac99f15e8cf0fa097
Francesco Refolli <f.refolli@campus.unimib.it>
2024-11-04 16:02:47 +0100

    locate GOP

--- src/main.c
+++ src/main.c
@@ -2,15 +2,26 @@
 #include <efi/efilib.h>
 #include <stdbool.h>
 
-EFI_STATUS
-EFIAPI
-efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
-{
-  InitializeLib(ImageHandle, SystemTable);
-  Print(L"Hello, world!\n");
+void loader_println(const wchar_t *msg) {
+  Print(L"EFI Loader :: \"%s\"\n\r", msg);
+}
 
-  Print(L"Die\n");
+EFI_STATUS EFIAPI efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+  // Initialize EFI::lib
+  InitializeLib(ImageHandle, SystemTable);
+  loader_println(L"Starting Session");
+
+  // Locating GOP protocol data (for VESA usage)
+  EFI_GUID gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+  EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+  EFI_STATUS Status = uefi_call_wrapper(BS->LocateProtocol, 3, &gopGuid, NULL, (void**)&gop);
+  if(EFI_ERROR(Status))
+    loader_println(L"Unable to locate GOP");
+  else
+    loader_println(L"GOP has been located");
+
+  // Hanging
+  loader_println(L"Ending Session");
   while(true);
   return EFI_SUCCESS;
 }
-
